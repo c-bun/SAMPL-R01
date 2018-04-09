2c9ff109c749ba894c641bd976a73252815aca8e
Colin Rathbun <colrath@gmail.com>
2018-04-09 16:47:40 -0700

    typeset all works but unecessarily runs some files.

--- typeset_all.sh
+++ typeset_all.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-xelatex Research_and_SA.tex
+for texfile in *.tex
+do
+  xelatex $texfile
+done
 
 for auxfile in *.aux
 do
     bibtex `basename $auxfile .aux`
 done
 
-xelatex Research_and_SA.tex
-xelatex Research_and_SA.tex
+for texfile in *.tex
+do
+  xelatex $texfile
+  xelatex $texfile
+done
 
 rm ./*.out
 rm ./*.aux
 rm ./*.bbl
 rm ./*.blg
 rm ./*.log
-
-open Research_and_SA.pdf
